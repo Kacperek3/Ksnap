cd6e1398650d650aec813fe5d939222bb2e8931c
Kacperek3 <kapisztuka1@gmail.com>
2026-03-24 14:33:15 +0100

    feat: first with initial project structure (doxygen, makefile, etc.)

---
+++ Ksnap-engine/src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from Ksync \n");
+
+    return 0;
+}
---
+++ Ksnap-engine/src/victims_programs/counter.c
@@ -0,0 +1,30 @@
+/**
+ * @file counter.c
+ * @author Kacperek3
+ * @date 24/03/2026
+ * @brief Simple program that counts from 0 to 99 with 1 sec pause.
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ ** To speed up the PID search, it displays it at the beginning of the program
+ * @return Return 0 when index reach 99.
+ */
+
+int main() {
+    int own_pid = getpid();
+
+    printf("-------------------------------\n");
+    printf("|        PID is %d        | \n", own_pid);
+    printf("-------------------------------\n\n");
+
+    for (int i = 0; i < 100; i++) {
+        printf("Index is: %d \n", i);
+        sleep(1);
+    }
+
+    return 0;
+}
