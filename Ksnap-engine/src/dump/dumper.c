327827b65bf0d5fecfced94c59d10218be81b4c3
kacper sztuka <kapisztuka1@gmail.com>
2026-04-25 20:28:00 +0200

    feat(dumper.c): saving foreign process regs to file

--- Ksnap-engine/src/dump/dumper.c
+++ Ksnap-engine/src/dump/dumper.c
@@ -1,4 +1,5 @@
 #include <sys/ptrace.h> // for ptrace
+#include <sys/user.h>   // for user_regs_struct
 
 #include <sys/types.h>
 #include <sys/wait.h> // for waitpid
@@ -9,16 +10,30 @@
 
 void dump(ksnap_config_t config) {
     int status;
+    struct user_regs_struct regs;
 
     ptrace(PTRACE_SEIZE, config.pid, NULL,
            NULL); // attach process to our program
     ptrace(PTRACE_INTERRUPT, config.pid, NULL, NULL); // stoping tracee
 
     waitpid(config.pid, &status, 0);
-    printf("process should be stopped for 5 sec\n");
+    ptrace(PTRACE_GETREGS, config.pid, NULL, &regs);
 
-    sleep(5);
+    FILE *file_handle;
+    file_handle = fopen("save/regs.bin", "wb+");
+    if (file_handle == NULL) {
+        // handle it later
+        printf("problem here \n");
+        return;
+    }
 
-    printf("process unfreezing \n");
-    ptrace(PTRACE_DETACH, config.pid, NULL, NULL);
+    int flag = fwrite(&regs, sizeof(struct user_regs_struct), 1, file_handle);
+
+    if (!flag) {
+        printf("write operation failure");
+    } else {
+        printf("write operation succesfully");
+    }
+
+    fclose(file_handle);
 }
